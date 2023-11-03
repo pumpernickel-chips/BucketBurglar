2655a007c04bd294e14e729b2c94526684c46911
Yuliia Synytska <evroconsult.ys@gmail.com>
2023-11-03 17:11:06 -0400

    add constructors and empty methods to classes Room and Player

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,44 @@
+
+
+import java.util.Deque;
+/**
+ * @author Yuliia Synytska
+ */
 public class Player {
+
+    private int currentScore;
+    private String name;
+    private Deque<String> roomKeys;
+
+    //default constructor method
+    public Player(){
+
+    }
+
+    //constructor method to set values for class attributes
+    public Player(int currentScore, String name, Deque<String> roomKeys) {
+        this.currentScore = currentScore;
+        this.name = name;
+        this.roomKeys = roomKeys;
+    }
+
+    //method for a player than player can use to join the multi player game
+    public void joinGame(){
+
+    }
+
+    //method for the player to leave the game anytime
+    public void leaveGame(){
+
+    }
+
+    //method for the player to decide a path to move in/around the castle/rooms
+    public void decidePath(){
+
+    }
+
+
+
+
 }
+
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,27 @@
+
+
+import java.awt.Dimension;
+/**
+ * @author Yuliia Synytska
+ */
 public class Room {
+
+    private Dimension dimension;
+    private boolean isEmpty;
+
+    //default method for Room
+    public Room() {
+        isEmpty = false;
+    }
+
+    //parameterized method to set class attributes for Room
+    public Room(Dimension dimension, boolean isEmpty) {
+        this.dimension = dimension;
+        this.isEmpty = isEmpty;
+    }
+
+    //method to generate loot in a Room so that players can grab it
+    public void generateLoot(){
+
+    }
 }
