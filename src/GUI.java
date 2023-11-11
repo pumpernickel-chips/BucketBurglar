85c4caee54841b6fd21c3f1eb61846bfe57adb6b
Naomi <104791815+pumpernickel-chips@users.noreply.github.com>
2023-11-11 18:56:55 -0500

    Wrote some GUI, mainly a system for JButtons that have their actions handled in HashGame but have pointers in GUI to implement the visuals.

--- src/GUI.java
+++ src/GUI.java
@@ -1,12 +1,22 @@
-
-
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
+
 /**
  * @author Naomi Coakley
  */
-public class GUI extends JPanel {
-    JFrame frame;
+public class GUI extends JPanel{
+    private String name;
+    private boolean[] playerJoined;
+    private int h, w;
+    private final static Font buttonFont = new Font("Hevetica", Font.BOLD, 18);
+    private JFrame frame;
+    private JPanel menu;
+    private JPanel game;
+    private JButton[] addPlayer;
+    private static final Color[] playerColors = new Color[]{new Color(28, 129, 248), new Color(234, 57, 132), new Color(18, 155, 26), new Color(210, 108, 29)};
+
+
     /**
      * Default zero-args constructor, passes default title to complete constructor
      * */
@@ -19,20 +29,101 @@ public class GUI extends JPanel {
      * */
     public GUI(String name){
         super(new GridBagLayout(), true);
-
-        initializeGUI(name, 1280, 800);
+        this.name = name;
     }
 
-    public void initializeGUI(String name, int height, int width){
-        frame = new JFrame(name);
-        frame.setPreferredSize(new Dimension(height, width));
-        this.setMinimumSize(new Dimension(height, width));
+    public void initializeGUI(int height, int width){
+        this.h = height;
+        this.w = width;
+
+        playerJoined = new boolean[]{false, false, false, false};
+        this.setMinimumSize(new Dimension(h, w));
         this.setBackground(new Color(43, 43, 44, 255));
-        frame.add(this);
+
+        showMenu();
+
+        frame = new JFrame(name);
+        frame.setPreferredSize(new Dimension(h, w));
+        frame.setContentPane(menu);
         frame.setVisible(true);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public void showMenu(){}
+    public void linkInputs(List<JButton> inputs){
+        addPlayer = new JButton[4];
+        for (JButton input : inputs) {
+            switch(input.getActionCommand()) {
+                case "P1" :
+                    addPlayer[0] = input;
+                case "P2" :
+                    addPlayer[1] = input;
+                case "P3" :
+                    addPlayer[2] = input;
+                case "P4" :
+                    addPlayer[3] = input;
+            }
+        }
+    }
+    public void showMenu(){
+        menu = new JPanel(true);
+        menu.setPreferredSize(new Dimension(h, w));
+        menu.setMinimumSize(new Dimension(h, w));
+        menu.setBackground(new Color(43, 43, 44, 255));
+        menu.setLayout(new GridBagLayout());
+        GridBagConstraints grd = new GridBagConstraints();
+
+
+
+
+        int playerNum = 0;
+
+        for(JButton b : addPlayer){
+            playerNum++;
+            b.setText("ENTER PLAYER " + playerNum);
+            b.setBackground(playerNum == 1? Color.WHITE : Color.DARK_GRAY);
+            b.setForeground(playerNum == 1? Color.DARK_GRAY : Color.LIGHT_GRAY);
+            b.setFocusPainted(false);
+            b.setBorder(BorderFactory.createLineBorder((playerNum == 1? playerColors[playerNum - 1] : Color.LIGHT_GRAY), 3, false));
+            b.setFont(buttonFont);
+        }
+        //constraints applying to all addPlayer buttons
+        grd.weighty = 0.5;
+        grd.weightx = 0.5;
+        grd.ipadx = 10;
+        grd.ipady = 10;
+        grd.insets = new Insets (5,5,5,5);
+        grd.gridy = 2;
+
+        grd.gridx = 1;
+        menu.add(addPlayer[0], grd);
+        grd.gridx = 3;
+        menu.add(addPlayer[1], grd);
+        grd.gridx = 5;
+        menu.add(addPlayer[2], grd);
+        grd.gridx = 7;
+        menu.add(addPlayer[3], grd);
+    }
     public void showMap(){}
+    public boolean isPlayerJoined() {
+        if(playerJoined[0]){
+            return true;
+        }
+        return false;
+    }
+    public boolean isPlayerJoined(int playerNum) {
+        return playerNum <= 4 && playerNum > 0 && this.playerJoined[playerNum - 1];
+    }
+    public void updatePlayerButtons(boolean joined, int playerNum) {
+        this.playerJoined[playerNum-1] = joined;
+        addPlayer[playerNum - 1].setBackground(joined? playerColors[playerNum - 1] : Color.WHITE);
+        addPlayer[playerNum - 1].setForeground(joined? Color.WHITE : Color.DARK_GRAY);
+        addPlayer[playerNum - 1].setBorder(BorderFactory.createLineBorder((joined? Color.WHITE : playerColors[playerNum - 1]), 3, false));
+        addPlayer[playerNum - 1].setText(joined? ("PLAYER " + playerNum + " JOINED") : ("ENTER PLAYER " + playerNum));
+
+        if(playerNum<4){
+            addPlayer[playerNum].setBackground(joined? Color.WHITE : Color.DARK_GRAY);
+            addPlayer[playerNum].setForeground(joined? Color.DARK_GRAY : Color.LIGHT_GRAY);
+            addPlayer[playerNum].setBorder(BorderFactory.createLineBorder((joined? playerColors[playerNum] : Color.LIGHT_GRAY), 3, false));
+        }
+    }
 }
--- src/HashGame.java
+++ src/HashGame.java
@@ -1,27 +1,31 @@
-import java.util.Iterator;
-import java.util.Map;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
 
 /**
  * @author Naomi Coakley
  */
-public class HashGame {
+public class HashGame implements ActionListener {
     public static final int STAY = 0, NORTH = 1, EAST = 2, SOUTH = 3, WEST = 4, N_EAST = 5, N_WEST = 6, S_EAST = 7, S_WEST = 8;
     public static final int[] DIRECTIONS = new int[]{0, 1 ,2 , 3, 4, 5, 6, 7, 8};
-
+    private GUI gui;
     private EntityTable entities;
     private FloorPlan level;
     private Iterator<Map.Entry<Integer, GameEntity>> iterEnt;
     public HashGame(){
-        GUI g = new GUI();
+        gui = new GUI();
         this.entities = new EntityTable();
         level = new FloorPlan(entities);
         iterEnt = entities.entrySet().iterator();
+        gui.linkInputs(initializeInputs());
+        gui.initializeGUI(1280, 800);
         trapTreasures();
         setPaths();
     }
     public static void main(String[] args){
         HashGame test = new HashGame();
-    };
+    }
     public void setPaths() {
         while(iterEnt.hasNext()){
             GameEntity p = iterEnt.next().getValue();
@@ -31,7 +35,6 @@ public class HashGame {
             }
         }
     }
-
     public void trapTreasures(){
         while(iterEnt.hasNext()){
             GameEntity t = iterEnt.next().getValue();
@@ -42,6 +45,65 @@ public class HashGame {
     }
     public void createPlayers(){}
     public void movePlayer(int posX, int posY, int direction){}
-
     public void initializeMap(){}
+    public List<JButton> initializeInputs(){
+        JButton addP1 = new JButton(), addP2 = new JButton(), addP3 = new JButton(), addP4 = new JButton(),
+                start = new JButton(), end = new JButton(), quit = new JButton();
+        List<JButton> inputs = new ArrayList<JButton>(Arrays.asList(addP1, addP2, addP3, addP4, start, end, quit));
+        for(JButton b : inputs){
+            b.addActionListener(this);
+        }
+        //add players
+        addP1.setActionCommand("P1");
+        addP2.setActionCommand("P2");
+        addP3.setActionCommand("P3");
+        addP4.setActionCommand("P4");
+        //start, quit, retry
+        start.setActionCommand("start");
+        end.setActionCommand("end");
+        quit.setActionCommand("quit");
+        for(JButton b : inputs){
+            System.out.print(b.getActionCommand() + " ");
+        }
+        return inputs;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch(e.getActionCommand()){
+            case "P1":
+                if(!gui.isPlayerJoined(1) && !gui.isPlayerJoined(2)){
+                    gui.updatePlayerButtons(true, 1);
+                }else if(gui.isPlayerJoined(1) && !gui.isPlayerJoined(2)){
+                    gui.updatePlayerButtons(false, 1);
+                }
+                break;
+            case "P2":
+                if(gui.isPlayerJoined(1)) {
+                    if (!gui.isPlayerJoined(2) && !gui.isPlayerJoined(3)) {
+                        gui.updatePlayerButtons(true, 2);
+                    } else if (gui.isPlayerJoined(2) && !gui.isPlayerJoined(3)) {
+                        gui.updatePlayerButtons(false, 2);
+                    }
+                }
+                break;
+            case "P3":
+                if(gui.isPlayerJoined(1) && gui.isPlayerJoined(2)) {
+                    if (!gui.isPlayerJoined(3) && !gui.isPlayerJoined(4)) {
+                        gui.updatePlayerButtons(true, 3);
+                    } else if (gui.isPlayerJoined(3) && !gui.isPlayerJoined(4)) {
+                        gui.updatePlayerButtons(false, 3);
+                    }
+                    break;
+                }
+            case "P4":
+                if(gui.isPlayerJoined(1) && gui.isPlayerJoined(2) && gui.isPlayerJoined(3)) {
+                    if (!gui.isPlayerJoined(4)) {
+                        gui.updatePlayerButtons(true, 4);
+                    } else if (gui.isPlayerJoined(1)) {
+                        gui.updatePlayerButtons(false, 4);
+                    }
+                }
+                break;
+        }
+    }
 }
