5e83056097286fc2a9971c3b451bb078c3674bf8
Naomi <104791815+pumpernickel-chips@users.noreply.github.com>
2023-11-12 14:44:35 -0500

    Added some FloorPlan methods and HashGame functionality. Start button functionality in progress (throws a bunch of NullPointerExceptions if you click Start at the moment but ~it's fine~), made the game run in fullscreen just for fun.

--- src/FloorPlan.java
+++ src/FloorPlan.java
@@ -1,12 +1,47 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Random;
 
 public class FloorPlan extends JPanel {
+    private int minRooms;
     private EntityTable entities;
+    private Iterator<Map.Entry<Integer, GameEntity>> iteRoom;
     public FloorPlan(){
-        this(new EntityTable());
+        this(5, new EntityTable());
     }
-    public FloorPlan(EntityTable entities){
+    public FloorPlan(int minRooms, EntityTable entities){
         super(true);
+        this.minRooms = minRooms;
+        this.entities = entities;
+        //generateRooms();
+        //arrangeRooms();
+        //drawHallways();
+    }
+    public void generateRooms(){
+        Random rand = new Random();
+        int need = minRooms + rand.nextInt(minRooms+2);
+        Room.maxSize = new Dimension (321, 161);
+        while(entities.getRoomCount() < need || rand.nextBoolean()) {
+            Room rm = new Room(1,1, 64+rand.nextInt(256), 32+rand.nextInt(128), true);
+
+            entities.put(((Room) rm).hashCode(), rm);
+        }
+    }
+    public void arrangeRooms(){
+        while(iteRoom.hasNext()){
+            GameEntity e = iteRoom.next().getValue();
+            if(e instanceof Room){
+                Room r = (Room) e;
+                Dimension s = r.getRoomSize();
+                Point2D o = r.getOrigin();
+
+            }
+        }
+    }
+    public void drawHallways(){
 
     }
 }
