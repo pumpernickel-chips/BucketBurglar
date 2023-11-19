36fc507472c9084a6d00ad4b199f9ef43a2d8140
Naomi <104791815+pumpernickel-chips@users.noreply.github.com>
2023-11-19 05:10:19 -0500

    Forgot the GameEntity comments lmao

--- src/GameEntity.java
+++ src/GameEntity.java
@@ -1,6 +1,17 @@
 import java.awt.*;
-
+/**
+ * Allows hashing implementation to be demonstrated with larger quantity by unifying the game's assets as one interface
+ * @author Naomi Coakley
+ * */
 public interface GameEntity {
+    /**
+     * Gets the sprite
+     * @return the sprite's Shape (cast to Ellipse2D and Rectangle2D)
+     * */
     Shape getSprite();
+    /**
+     * Moves the sprite
+     * @return whether the sprite has arrived at it's next destination.
+     * */
     boolean moveSprite(double stepInterval);
 }
